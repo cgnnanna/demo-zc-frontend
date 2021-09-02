7aa7017df08f8641628ed6e982238919b320af1a
cgnnanna <cgnnanna@gmail.com>
2021-09-02 11:42:51 +0100

    created a project srtucture for login page

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Login from './pages/login'
+import './styles/globals.css'
+// import 'tailwindcss/tailwind.css'
+
+const App = () => {
+  return (
+    <Login />
+  );
+}
+
+export default App;
